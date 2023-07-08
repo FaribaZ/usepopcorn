2a4a8fa8288fcf9d0554a61ac0f9d3df0d3669eb
FaribaZ <111224004+FaribaZ@users.noreply.github.com>
2023-07-08 08:24:59 +0330

    add useref

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import React from "react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating
+//         maxRating={10}
+//         color="blue"
+//         size={30}
+//         onSetRating={setMovieRating}
+//       />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating maxRating={5} />
+    <StarRating maxRating={10} color="red" size={30} />
+    <Test /> */}
   </React.StrictMode>
 );
 
